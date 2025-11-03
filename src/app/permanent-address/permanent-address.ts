ef432193f4ac9fb3649f74ec68b4bb7b18629e8a
Muhammad-Mehmood773 <muhammadmehmood773@gmail.com>
2025-11-03 16:44:48 +0500

    new changes

--- src/app/permanent-address/permanent-address.ts
+++ src/app/permanent-address/permanent-address.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SHARED_IMPORTS } from '../shared/ng-zorro-imports';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { getNzErrorMessage } from '../shared/helpers/validation-messages';
 
 @Component({
   selector: 'app-permanent-address',
@@ -10,8 +11,43 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './permanent-address.css',
 })
 export class PermanentAddress implements OnInit {
-  constructor() { }
+
+  permanentAddressForm!: FormGroup;
+
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.permanentAddressForm = this.fb.group({
+      countryId: ['',[Validators.required]],
+      provinceId: ['',[Validators.required]],
+      cityId: ['',[Validators.required]],
+      postalCode: [''],
+      address: ['',[Validators.maxLength(300)]],
+    });
   }
+
+
+  getError(controlName: string): string {
+    const control = this.permanentAddressForm.get(controlName);
+    return getNzErrorMessage(control, controlName);
+  }
+
+  markFormTouched(): void {
+    Object.values(this.permanentAddressForm.controls).forEach((ctrl) => {
+      ctrl.markAsTouched();
+      ctrl.updateValueAndValidity();
+    });
+  }
+
+  onSubmit(): void {
+    if (this.permanentAddressForm.invalid) {
+      this.markFormTouched();
+      return;
+    }
+
+    console.log('Form Data:', this.permanentAddressForm.value);
+  }
+
+
 }
--- src/app/present-address/present-address.ts
+++ src/app/present-address/present-address.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SHARED_IMPORTS } from '../shared/ng-zorro-imports';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { getNzErrorMessage } from '../shared/helpers/validation-messages';
 
 @Component({
   selector: 'app-present-address',
@@ -12,9 +13,42 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class PresentAddress implements OnInit {
 
 
-  constructor() { }
+  presentAddressForm!: FormGroup;
+
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.presentAddressForm = this.fb.group({
+      countryId: ['',[Validators.required]],
+      provinceId: ['',[Validators.required]],
+      cityId: ['',[Validators.required]],
+      postalCode: [''],
+      address: ['',[Validators.maxLength(300)]],
+    });
   }
+
+
+  getError(controlName: string): string {
+    const control = this.presentAddressForm.get(controlName);
+    return getNzErrorMessage(control, controlName);
+  }
+
+  markFormTouched(): void {
+    Object.values(this.presentAddressForm.controls).forEach((ctrl) => {
+      ctrl.markAsTouched();
+      ctrl.updateValueAndValidity();
+    });
+  }
+
+  onSubmit(): void {
+    if (this.presentAddressForm.invalid) {
+      this.markFormTouched();
+      return;
+    }
+
+    console.log('Form Data:', this.presentAddressForm.value);
+  }
+
 
 }
