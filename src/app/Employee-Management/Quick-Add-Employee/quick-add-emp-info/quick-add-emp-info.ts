bde696bb1c405d511b723dce676cd319b5d6d1cc
Muhammad-mehmood773 <muhammadmehmood773@gmail.com>
2025-11-23 13:11:26 +0500

    update

--- src/app/Employee-Management/Quick-Add-Employee/quick-add-emp-info/quick-add-emp-info.ts
+++ src/app/Employee-Management/Quick-Add-Employee/quick-add-emp-info/quick-add-emp-info.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SHARED_IMPORTS } from '../../../shared/theme/ng-zorro-imports';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { getNzErrorMessage } from '../../../shared/helpers/validation-messages';
-import { Skeleton } from "../../../shared/components/skeleten/skeleten";
+import { Skeleton } from "../../../shared/components/skeleton/skeleten";
 
 @Component({
   selector: 'app-quick-add-emp-info',
