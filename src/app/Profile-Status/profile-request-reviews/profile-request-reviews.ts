897b5cbd15d6a88ddc0c2ddc0bb17f0e85e11d40
Muhammad-Mehmood773 <muhammadmehmood773@gmail.com>
2025-11-27 21:30:05 +0500

    test

--- src/app/Profile-Status/profile-request-reviews/profile-request-reviews.ts
+++ src/app/Profile-Status/profile-request-reviews/profile-request-reviews.ts
@@ -13,6 +13,13 @@ export class ProfileRequestReviews implements OnInit {
 
   route = inject(ActivatedRoute);
   id = this.route.snapshot.paramMap.get('id');
+  employee = {
+    name: 'John Doe',
+    code: 'EMP007',
+    submittedOn: '07-Jul-2025',
+    requestedSections: ['Personal Info', 'Academic Info'],
+     image: 'https://picsum.photos/80'
+  };
 
   constructor() { }
 
